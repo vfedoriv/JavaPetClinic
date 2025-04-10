28529e2ac3e16b4b774e063e2c14e0729f521804
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2025-04-10 18:51:06 +0300

    initialize JavaPetClinic project with Maven setup and basic application structure

---
+++ src/main/java/io/github/vfedoriv/javapetclinic/JavaPetClinicApplication.java
@@ -0,0 +1,12 @@
+package io.github.vfedoriv.javapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPetClinicApplication
+{
+  public static void main(String[] args) {
+    SpringApplication.run(JavaPetClinicApplication.class, args);
+  }
+}
---
+++ src/test/java/io/github/vfedoriv/javapetclinic/JavaPetClinicApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.vfedoriv.javapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPetClinicApplicationTests
+{
+  @Test
+  void contextLoads() {
+  }
+}
