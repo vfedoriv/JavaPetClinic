de4f63520220ca3a467948eee895e17d30dbab44
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2025-04-12 20:48:02 +0300

    add comments

--- src/main/java/io/github/vfedoriv/javapetclinic/JavaPetClinicApplication.java
+++ src/main/java/io/github/vfedoriv/javapetclinic/JavaPetClinicApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaPetClinicApplication
 {
+  // note: this is a Spring Boot application
   public static void main(String[] args) {
     SpringApplication.run(JavaPetClinicApplication.class, args);
   }
